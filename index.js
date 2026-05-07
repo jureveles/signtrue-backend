834fe17b6c4375f97e6e4cd8daaa27fb9de23373
Dr. Ulises Reveles <102607165+jureveles@users.noreply.github.com>
2026-05-07 14:59:49 -0400

    Update index.js - added calendar line to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
+const { createCalendarEvent } = require('./google_calendar');
 
 const app = express();
 app.use(cors());
